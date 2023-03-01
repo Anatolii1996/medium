b143a8ab2938f335e43d41219064fb91c00a95cb
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-01 10:07:06 +0200

    получаю из firebase комнаты и юзеров

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
 /* eslint-disable */
 import React from 'react';
 import './App.scss';
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import { db } from './firebase';
 // import axios from 'axios';
 // import {   doc, setDoc } from 'firebase/firestore';
-// import { db } from './firebase';
 
 function App() {
-  //Получение массива комнат
-  // const [rooms, SetRooms] = useState([]);
+  // <>
+  {/* Получение массива комнат
+const [rooms, SetRooms] = useState([]);
 
-  //Запись массива комнат в Firebase
-  // const createAccount =  () => {
-  //    rooms.map((el) => {
-  //    setDoc(doc(db, "Rooms", el.id), {...el})      
+  Запись массива комнат в Firebase
+  Fconst createAccount =  () => {
+     rooms.map((el) => {
+     setDoc(doc(db, "Rooms", el.id), {...el})      
 
-  //   })
+    })
 
-  // }
+  }
 
 
-  //Запрос данных из localhost
-  // useEffect(() => {
-  //   axios("http://localhost:3001/posts")
-  //     .then(resp => {
-  //       SetRooms(resp.data[0].Rooms)
-  //     }
-  //     )
+  Запрос данных из localhost
+  useEffect(() => {
+    axios("http://localhost:3001/posts")
+      .then(resp => {
+        SetRooms(resp.data[0].Rooms)
+      }
+      )
 
-  // }, []);
+  }, []); */}
 
+  // </>
+  const [rooms, setRooms] = useState([]);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const q = query(collection(db, "Rooms"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let roomsArr = [];
+      querySnapshot.forEach((doc) => {
+        roomsArr.push({ ...doc.data(), id: doc.id })
+      })
+      setRooms(roomsArr);
+
+    })
+    return () => unsubscribe
+  }, []);
+  useEffect(() => {
+    const q = query(collection(db, "Accounts"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let usersArr = [];
+      querySnapshot.forEach((doc) => {
+        usersArr.push({ ...doc.data(), id: doc.id })
+      })
+      setUsers(usersArr);
+
+    })
+    return () => unsubscribe
+  }, []);
 
   return (
     <div className="App">
@@ -40,6 +69,8 @@ function App() {
       }}>Получить данные</button>
       
       {console.log(rooms)} */}
+      {console.log(rooms)}
+      {console.log(users)}
     </div>
   );
 }
