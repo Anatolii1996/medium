a32d8ed808917454e08601d30314d2a48932913c
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-07 19:55:17 +0200

    дополнил верстку

--- src/pages/SingleRoomPage.jsx
+++ src/pages/SingleRoomPage.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { getRoomsState } from "../redux/selectors";
 import { Link, useParams } from "react-router-dom";
-import { HomeOutlined } from "@ant-design/icons";
-import { Carousel } from "antd";
+import { HomeOutlined, CheckOutlined } from "@ant-design/icons";
+import { Carousel, Descriptions, List } from "antd";
 
 const SingleRoomPage = () => {
   const { id } = useParams();
@@ -23,19 +23,48 @@ const SingleRoomPage = () => {
         <p>Back Home</p>
       </Link>
       <div className="single_center">
-          <Carousel className="room_img" autoplay>
-          {currentRoom.gallery.map((imageUrl) => <img key={imageUrl} src={imageUrl} alt={currentRoom.type} className="slider-image" />)}
-          </Carousel>
-          <div className="description">
-            <h3>{`Room ${currentRoom.number}`}</h3>
-            <p>Type: <span>{currentRoom.type}</span></p>
-            <p>Occupancy: <span>{currentRoom.occupancy}</span></p>
-            <p>Price: <span>{currentRoom.price}</span></p>
-            <p>Guest: <span>{currentRoom.guest}</span></p>
-           
-
-
-          </div>
+        <Carousel className="room_img" autoplay>
+          {currentRoom.gallery.map((imageUrl) => (
+            <img
+              key={imageUrl}
+              src={imageUrl}
+              alt={currentRoom.type}
+              className="slider-image"
+            />
+          ))}
+        </Carousel>
+        <div className="description">
+          <h3>{`Room ${currentRoom.number}`}</h3>
+          <p>
+            Type: <span>{currentRoom.type}</span>
+          </p>
+          <p>
+            Occupancy: <span>{currentRoom.occupancy}</span>
+          </p>
+          <p>
+            Price: <span>{currentRoom.price}</span>
+          </p>
+          <p>
+            Guest: <span>{currentRoom.guest}</span>
+          </p>
+        </div>
+        <div className="feature">
+          <Descriptions layout="vertical" labelStyle={{ fontWeight: 700, color: "black", fontSize: 20 }}>
+            <Descriptions.Item label="Features">
+              <List
+                size="small"
+                dataSource={currentRoom.features}
+                renderItem={(item) => (
+                  <List.Item>
+                    <CheckOutlined />
+                    &nbsp;
+                    {item}
+                  </List.Item>
+                )}
+              />
+            </Descriptions.Item>
+          </Descriptions>
+        </div>
       </div>
     </div>
   );
