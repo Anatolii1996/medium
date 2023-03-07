c2dc824a76e5f72b0e5a62c1f81ac5444c7d8903
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-07 19:34:14 +0200

    сверстал описание комнаты

--- src/pages/SingleRoomPage.jsx
+++ src/pages/SingleRoomPage.jsx
@@ -27,7 +27,14 @@ const SingleRoomPage = () => {
           {currentRoom.gallery.map((imageUrl) => <img key={imageUrl} src={imageUrl} alt={currentRoom.type} className="slider-image" />)}
           </Carousel>
           <div className="description">
-            <h2>{`Room ${currentRoom.number}`}</h2>
+            <h3>{`Room ${currentRoom.number}`}</h3>
+            <p>Type: <span>{currentRoom.type}</span></p>
+            <p>Occupancy: <span>{currentRoom.occupancy}</span></p>
+            <p>Price: <span>{currentRoom.price}</span></p>
+            <p>Guest: <span>{currentRoom.guest}</span></p>
+           
+
+
           </div>
       </div>
     </div>
