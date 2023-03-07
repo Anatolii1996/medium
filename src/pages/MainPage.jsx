7580cee2d7f903a21cd747ee51f8b2ecc3528a25
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-07 08:09:57 +0200

    добавил сортировку по гостям

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,70 +1,16 @@
 /* eslint-disable */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 // import MyTable from "../components/MyTable";
 import { useSelector } from "react-redux";
 import { Table } from "antd";
-const columns = [
-  {
-    title: "Number",
-    dataIndex: "number",
-    width: "20%",
-  },
-  {
-    title: "Type",
-    dataIndex: "type",
-    filters: [
-      {
-        text: "Standart",
-        value: "standard",
-      },
-      {
-        text: "Suite",
-        value: "suite",
-      },
-      {
-        text: "Delux",
-        value: "delux",
-      },
-    ],
-    onFilter: (value, record) => record.type.indexOf(value) === 0,
-  
-    width: "20%",
-  },
-  {
-    title: "Occupancy",
-    dataIndex: "occupancy",
-    filters: [
-      {
-        text: 2,
-        value: 2,
-      },
-      {
-        text: 3,
-        value: 3,
-      },
-      {
-        text: 4,
-        value: 4,
-      },
-    ],
-    onFilter: (value, record) => record.occupancy === value,
-    width: "20%",
-  },
-  {
-    title: "Price",
-    dataIndex: "price",
-    sorter: (a, b) => a.price - b.price,
-  },
-  {
-    title: "Guest",
-    dataIndex: "guest",
-  },
-];
+import { getRoomsState } from "../redux/selectors";
 
 
 const MainPage = () => {
-  const { rooms } = useSelector((state) => state);
+  const  rooms  = useSelector(getRoomsState);
   const [roomsItems, setRoomsItems] = useState([]);
+
+  const [isChecked, setIsChecked] = useState(false);
 
   useEffect(() => {
     const roomsArr = Object.entries(rooms)
@@ -79,12 +25,83 @@ const MainPage = () => {
     setRoomsItems(roomsArr);
   }, [rooms]);
   
+  const guestsOptions = useMemo(() => (!isChecked
+    ? rooms
+      .filter((room) => room.guest)
+      .map((room) => ({ text: room.guest, value: room.guest }))
+    : []
+  ), [rooms, isChecked]);
+
+  const columns = [
+    {
+      title: "Number",
+      dataIndex: "number",
+      width: "20%",
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+      filters: [
+        {
+          text: "Standart",
+          value: "standard",
+        },
+        {
+          text: "Suite",
+          value: "suite",
+        },
+        {
+          text: "Delux",
+          value: "delux",
+        },
+      ],
+      onFilter: (value, record) => record.type.indexOf(value) === 0,
+    
+      width: "20%",
+    },
+    {
+      title: "Occupancy",
+      dataIndex: "occupancy",
+      filters: [
+        {
+          text: 2,
+          value: 2,
+        },
+        {
+          text: 3,
+          value: 3,
+        },
+        {
+          text: 4,
+          value: 4,
+        },
+      ],
+      onFilter: (value, record) => record.occupancy === value,
+      width: "20%",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      sorter: (a, b) => a.price - b.price,
+    },
+    {
+      title: "Guest",
+      dataIndex: "guest",
+      filters: guestsOptions,
+      onFilter: (text, record) => record.guest.startsWith(text),
+    },
+  ];
   
   return (
+    <>
+     {
+      console.log(Object.entries(rooms) )}
     <Table
       columns={columns}
       dataSource={roomsItems}
     />
+    </>
+   
    
   );
 };
