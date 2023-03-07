ea4ba394ac7653d3e697bceea83d4d73a6ee90a8
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-07 17:38:36 +0200

    верстаю сингл пейдж

--- src/pages/SingleRoomPage.jsx
+++ src/pages/SingleRoomPage.jsx
@@ -2,7 +2,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { getRoomsState } from "../redux/selectors";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { HomeOutlined } from "@ant-design/icons";
+import { Carousel } from "antd";
 
 const SingleRoomPage = () => {
   const { id } = useParams();
@@ -14,9 +16,21 @@ const SingleRoomPage = () => {
       currentRoom = el;
     }
   });
-  return <>
-  <h1>TEST</h1>
-  {console.log(rooms)}
-  </>;
+  return (
+    <div className="main_content single_page">
+      <Link to="/">
+        <HomeOutlined />
+        <p>Back Home</p>
+      </Link>
+      <div className="single_center">
+          <Carousel className="room_img" autoplay>
+          {currentRoom.gallery.map((imageUrl) => <img key={imageUrl} src={imageUrl} alt={currentRoom.type} className="slider-image" />)}
+          </Carousel>
+          <div className="description">
+            <h2>{`Room ${currentRoom.number}`}</h2>
+          </div>
+      </div>
+    </div>
+  );
 };
 export default SingleRoomPage;
