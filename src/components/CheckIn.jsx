0292ef17c0153be92a301e03b76053d86e52c81b
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-08 10:42:05 +0200

    поправил окно заселения

--- src/components/CheckIn.jsx
+++ src/components/CheckIn.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable */
 import React, { useState } from "react";
-import { Button, Modal } from "antd";
+import { Button, Modal, Checkbox, Form, Input } from "antd";
 
-const CheckIn = ({currentRoom}) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const showModal = () => {
-      setIsModalOpen(true);
-    };
-    const handleOk = () => {
-      setIsModalOpen(false);
-    };
-    const handleCancel = () => {
-      setIsModalOpen(false);
-    };
+const CheckIn = ({ currentRoom }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <>
@@ -20,11 +20,42 @@ const CheckIn = ({currentRoom}) => {
         Check in
       </Button>
       <Modal
-        title={`Do you really want to check in the room ${currentRoom.number} ?` }
+        title={`Do you really want to check in the room ${currentRoom.number} ?`}
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
       >
+        <p>Please enter your name</p>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 4,
+          }}
+          wrapperCol={{
+            span: 20,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+        </Form>
       </Modal>
     </>
   );
